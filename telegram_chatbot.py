cdcfaf9de4c949e16c9133ec3860e3af5630c4b6
YoonjeongYoo <opi2982@gmail.com>
2023-11-23 17:37:14 +0900

    주기를 10분으로 수정

--- telegram_chatbot.py
+++ telegram_chatbot.py
@@ -18,7 +18,7 @@ def job():
     id_channel = asyncio.run(bot.sendMessage(public_chat_name, text=str(now))).chat_id
 
 schedule = BackgroundScheduler()
-schedule.add_job(job, 'interval', minutes=30) # 30분마다 job()을 실행하도록 설정
+schedule.add_job(job, 'interval', minutes=10) # 10분마다 job()을 실행하도록 설정 -> 30분마다로 설정하려 했으나 계속되는 "raise TimedOut() from error telegram.error.TimedOut: Timed out" 오류로 10분으로 수정..
 schedule.start() #설정한 BackgroundScheduler 실행
 #schedule.every(1).minutes.do(job) -> 일반 scehdule로 사용했을시  "ValueError: a coroutine was expected, got <telegram.message.Message object at 0x000001E24A3500F8>" 발생
 
